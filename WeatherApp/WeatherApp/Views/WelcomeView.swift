66e8b188491736730f5127b4d003859adaf16f5c
AlinaSosnovich <alina.sosnovich@mail.ru>
2023-09-18 00:51:52 +0300

    Add Git Ignore

---
+++ WeatherApp/WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,43 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Sergei  Sosnovich on 13.09.23.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager:
+    LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the Weather App")
+                    .bold().font(.title)
+                
+                Text("Please share your corrent location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
